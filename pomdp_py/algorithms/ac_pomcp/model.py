0e7e7dead813360dcf002955962581a4ebb30b18
anishdiwan <anishhdiwan@gmail.com>
2023-12-25 14:49:23 +0100

    Update and test model

--- pomdp_py/algorithms/ac_pomcp/model.py
+++ pomdp_py/algorithms/ac_pomcp/model.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
+import random
 
 class LatentSpaceTf(nn.Module):
     """
@@ -24,7 +24,7 @@ class LatentSpaceTf(nn.Module):
 
         self.layers.insert(0, self.in_dim)
         self.layers.append(self.out_dim)
-        print(self.layers)
+        # print(self.layers)
 
         modules = []
         for i in range(len(self.layers) - 1):        
@@ -34,7 +34,6 @@ class LatentSpaceTf(nn.Module):
 
         self.encoder = nn.Sequential(*modules)
 
-        print(self.encoder)
 
 
     def forward(self, x):
@@ -46,25 +45,70 @@ class FiLMEmbeddings(nn.Module):
     """
     Compute the FiLM Embedding from a conditioning input. https://arxiv.org/pdf/1709.07871.pdf
 
+    Compute one set of scale, bias for each feature of the input
+    
     Args:
+        in_dim (int): dimensionality of the input
         cond_dim (int): dimensionality of the conditioning vector (cond)
         cond (tensor): conditioning tensor
         x (tensor): tensor to which conditioning is applied
     """
-    def __init__(self, cond_dim):
+    def __init__(self, in_dim, cond_dim):
         super().__init__()
         self.cond_dim = cond_dim
+        self.in_dim = in_dim
         self.film_encoder = nn.Sequential(
           nn.Mish(),
-          nn.Linear(self.cond_dim, 2),
+          nn.Linear(self.cond_dim, 2*in_dim),
+          nn.Unflatten(-1, (-1, 2))
         )
 
     def forward(self, x, cond):
         film_encoding = self.film_encoder(cond)
+
         scale = film_encoding[:,0]
         bias = film_encoding[:,1]
 
-        return (scale*x.transpose(0,1) + bias).transpose(0,1)
+        return scale*x + bias
+
+
+class BoolMask(nn.Module):
+    """
+    A very simple boolean mask.
+
+    Compare each element with the mask value and output a new boolean tensor
+    
+    Args:
+        maskval (float): The threshold to compare against
+    """
+    def __init__(self, maskval=0.5):
+        super().__init__()
+        self.maskval = maskval
+
+    def forward(self, x):
+        x = x < self.maskval
+        return x
+
+
+class ScaleMask(nn.Module):
+    """
+    A very simple output un-normalisation
+
+    Convert outputs in [0, 1] to some range [min, max]
+    
+    Args:
+        min (float): The min value of the output range
+        max (float): The max value of the output range
+    """
+    def __init__(self, minval, maxval):
+        super().__init__()
+        self.min = minval
+        self.max = maxval
+
+    def forward(self, x):
+        x = self.min + (self.max - self.min)*x
+        return x
+
 
 
 class MultiHeadAutoencoder(nn.Module):
@@ -81,30 +125,94 @@ class MultiHeadAutoencoder(nn.Module):
         encoder_hidden_layers (list): list of the number of neurons in the hidden layers of the encoder (in order of the layers)
         decoder_hidden_layers (2D list): 2D list of the number of neurons in the hidden layers of the decoder (in order of the layers)
         output_prob_predicter_hidden_layers (list): list of the number of neurons in the hidden layers of the output probability prediction head (in order of the layers)
+        batch_size (int): NOT the traditional definition of batch size. Here a batch is a belief subset. Each sample in a batch is a belief state
+        n (int): size of the environment    
     """
 
     def __init__(self, in_dim, latent_space_dim, cond_dim, out_dims, 
-        encoder_hidden_layers, decoder_hidden_layers, output_prob_predicter_hidden_layers):
+        encoder_hidden_layers, decoder_hidden_layers, output_prob_predicter_hidden_layers, batch_size, n):
         super().__init__()
         self.encoder = LatentSpaceTf(in_dim, encoder_hidden_layers, latent_space_dim)
-        self.film_encoder = FiLMEmbeddings(cond_dim)
-        self.output_heads = []
-        for i in out_dims:
-            self.output_heads.append(LatentSpaceTf(latent_space_dim, decoder_hidden_layers[i], out_dims[i]))
+        self.film_encoder = FiLMEmbeddings(latent_space_dim, cond_dim)
 
-        self.output_prob_predicter = LatentSpaceTf(latent_space_dim, output_prob_predicter_hidden_layers, batch_size)
+        # Output heads
+        self.output_heads = []
+        for i in range(len(out_dims)):
+            self.output_heads.append([LatentSpaceTf(latent_space_dim, decoder_hidden_layers[i], out_dims[i]), nn.Sigmoid()])
+
+        self.output_heads[0].append(ScaleMask(0, n-1))
+        # self.output_heads[1].append(BoolMask(0.5))
+        # self.output_heads[2].append(BoolMask(0.5))
+
+
+        for i in range(len(self.output_heads)):
+            self.output_heads[i] = nn.Sequential(*self.output_heads[i])
+
+
+        # Take in the whole batch of conditioned inputs in the latent space and compute new probabilities
+        self.output_prob_predicter = nn.Sequential(
+            LatentSpaceTf(batch_size*latent_space_dim, output_prob_predicter_hidden_layers, batch_size),
+            nn.Softmax(dim=0)
+            )
+
 
     def forward(self, x, cond):
         encoded_input = self.encoder(x)
         conditioned_input = self.film_encoder(encoded_input, cond)
+
         outputs = []
         for output_head in self.output_heads:
             outputs.append(output_head(conditioned_input))
 
+        output_probabilities = self.output_prob_predicter(conditioned_input.flatten())
+        concatenated_output = torch.cat((outputs), 1).to(torch.float)
 
-        # TODO: Apply constraints on the outputs. Softmax, upscaling etc.
+        return concatenated_output, output_probabilities
 
-        output_probabilities = self.output_prob_predicter(conditioned_input)
 
-        return torch.cat(outputs, output_probabilities)
+
+
+### TESTING ###
+def temp_generate_sample(n,k):
+    test_sample = torch.zeros((k+4))
+    test_sample[:2] = torch.randint(0, n-1, (2,))
+
+    mask = torch.rand(k+2)
+    mask[:-1] = mask[:-1] < 0.5
+    test_sample[2:] = mask
+
+    return test_sample
+
+def temp_generate_cond():
+    test_cond = torch.zeros((2*3))
+    for i in range(len(test_cond)):
+        if (i+1)%2 != 0:
+            test_cond[i] = torch.randint(0, 6, (1,))
+        else:
+            test_cond[i] = torch.rand(1)
+    return test_cond
+
+n, k = 5, 5
+b_size = 4
+device = 'cuda'
+batch = torch.zeros(b_size,k+4)
+for i in range(len(batch)):
+    batch[i] = temp_generate_sample(n,  k)
+
+cond = temp_generate_cond()
+
+
+batch.to(torch.device(device)).to(torch.float)
+cond.to(torch.device(device)).to(torch.float)
+
+print(f"Batch {b_size} x [posx posy | {k} x rocktype | terminal+state | prob]")
+print(batch)
+print("Cond [a, o, a, o ..]")
+print(cond)
+
+belief_net = MultiHeadAutoencoder(k+4, 128, 6, [2, k, 1], [64], [[64, 32], [64], [64, 32, 8]], [256, 126, 64, 8], b_size, n)
+out, prob = belief_net(batch, cond)
+
+print(out)
+print(prob)
 
